1afc43530764318663f6693256663d75f46835c0
Raghav Anandan <raghav@Raghavs-MacBook-Pro.local>
2018-03-24 18:15:57 -0700

    Socket.io implementation commit

--- server/server.js
+++ server/server.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.on('disconnect', () => {
+    console.log("Client disconencted");
+  })
+})
+
+server.listen(port, () => {
   console.log(`Started the app on port ${port}`);
 })
