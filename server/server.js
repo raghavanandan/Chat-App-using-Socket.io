5fdd19577b24bdd1b2d73cdf3273d67212df9d87
Raghav Anandan <raghav@Raghavs-MacBook-Pro.local>
2018-03-24 17:21:40 -0700

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const publicPath = path.join(__dirname, '../public');
+
+var app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+  console.log(`Started the app on port ${port}`);
+})
