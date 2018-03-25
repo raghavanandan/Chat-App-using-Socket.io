92e0c48b651b7f2f29861914a721d7d3b0c4e947
Raghav Anandan <raghav@Raghavs-MacBook-Pro.local>
2018-03-24 19:17:10 -0700

    Event handling using Socket.io

--- server/server.js
+++ server/server.js
@@ -16,6 +16,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: "server@server.com",
+    text: "Hey. What's up ?!",
+    creatAt: 123
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('Created Message', message);
+  })
+
   socket.on('disconnect', () => {
     console.log("Client disconencted");
   })
